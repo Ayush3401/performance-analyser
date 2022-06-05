9a5a0d7fb43a3db1e7439691511e8d75b531b945
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-06-05 23:13:45 +0530

    Add search, sort

--- src/components/ThirdPartyTable.js
+++ src/components/ThirdPartyTable.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Table from "./Table";
+import '../styles/ThirdPartyTable.css'
 
 function ThirdPartyTable({ id, headings, items }) {
   const [view, setView] = useState("entity");
   const [thirdPartyHeadings, setThirdPartyHeadings] = useState([...headings]);
   const [thirdPartyItems, setThirdPartyItems] = useState([...items]);
 
-  function changeView() {
-    if (view === "entity") {
+  function changeView(e) {
+    if (e.target.value === "script") {
       setView("script");
       setThirdPartyHeadings([
         { key: "url", text: "URL" },
@@ -18,7 +19,11 @@ function ThirdPartyTable({ id, headings, items }) {
       setThirdPartyItems(
         items.reduce((arr, item) => {
           if (item.subItems && item.subItems.items) {
-            return arr.concat(item.subItems.items.filter(item => item.url !== 'Other resources'));
+            return arr.concat(
+              item.subItems.items.filter(
+                (item) => item.url !== "Other resources"
+              )
+            );
           }
           return arr;
         }, [])
@@ -30,9 +35,13 @@ function ThirdPartyTable({ id, headings, items }) {
     }
   }
 
-  return (<>
-    <Table id={id} headings={thirdPartyHeadings} items={thirdPartyItems} />
-    <button onClick={changeView}>changeView</button>
+  return (
+    <>
+      <select value={view} onChange={changeView}>
+        <option value="entity">Entity View</option>
+        <option value="script">Script View</option>
+      </select>
+      <Table id={id} headings={thirdPartyHeadings} items={thirdPartyItems} />
     </>
   );
 }
